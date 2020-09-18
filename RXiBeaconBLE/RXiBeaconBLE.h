e361d08789252b8983cada33bb5f949b2195651d
Mohamed Hashem <mohamedhashem@xtrava.co>
2020-09-18 21:01:38 +0200

    Initial Commit

---
+++ RXiBeaconBLE/RXiBeaconBLE.h
@@ -0,0 +1,19 @@
+//
+//  RXiBeaconBLE.h
+//  RXiBeaconBLE
+//
+//  Created by mohamed hashem on 9/18/20.
+//  Copyright © 2020 mohamed hashem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RXiBeaconBLE.
+FOUNDATION_EXPORT double RXiBeaconBLEVersionNumber;
+
+//! Project version string for RXiBeaconBLE.
+FOUNDATION_EXPORT const unsigned char RXiBeaconBLEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXiBeaconBLE/PublicHeader.h>
+
+
